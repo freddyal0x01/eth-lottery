4ba23fef0ed3848549b28b2adff5f219b9945f8f
freddyal <freddyal0x01@gmail.com>
2025-01-26 22:43:38 -0500

    Added custom error for entering into a raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,13 +30,22 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH to enter the raffle!");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
